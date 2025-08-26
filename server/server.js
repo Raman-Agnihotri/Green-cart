7569cbb53f8b1a4512a7c4458505b01dae8f6a5f
Raman-Agnihotri <ramanagnihotrijma@gmail.com>
2025-08-27 04:26:28 +0530

    Update gitignore and server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ await connectCloudinary();
 // Allow multiple origins
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://green-cart-2pww.vercel.app/",
+  "https://green-cart-2pww.vercel.app",
 ];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
