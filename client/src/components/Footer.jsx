043aa6186a3326d4dbb4c50679f4c800b74ca4a8
Raman-Agnihotri <ramanagnihotrijma@gmail.com>
2025-10-23 18:30:38 +0530

    Updated Footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
                 </div>
             </div>
             <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
-                Copyright {new Date().getFullYear()} © GreatStack.dev All Right Reserved.
+                  Devloped By Raman Agnihotri.
             </p>
         </div>
     );
